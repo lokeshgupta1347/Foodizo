dadbf043e0d4cb17ea3e6cf94b61ed0bef753f9b
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-06-15 09:02:52 +0530

    first commit

---
+++ services/auth/src/middlewares/trycatch.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response,RequestHandler } from "express";
+
+export const TryCatch = (handler : RequestHandler):RequestHandler => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await handler(req,res,next);
+            
+        } catch (error:any) {
+            res.status(500).json({
+                message:error.message
+            })
+            
+        }
+   
+    }  
+};
